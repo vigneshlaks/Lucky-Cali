ced6ca194ba1aef193f32732770ae640a017f0df
Vignesh Lakshmanan <49462691+weekjeff@users.noreply.github.com>
2024-10-09 00:20:24 -0400

    added site values

--- frontend/src/components/shared/api.jsx
+++ frontend/src/components/shared/api.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.auth.localhost',
+  baseURL: 'https://api.lucky-cali.com',  
   headers: {
     'Content-Type': 'application/json',
   },
